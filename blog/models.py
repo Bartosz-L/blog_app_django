db1c82fc324a084cd01f8b42ad48b58a169ed61f
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2020-01-07 09:15:19 +0100

    added urls in urls file; added absolute url method in models file

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 # Create your models here.
@@ -27,6 +28,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
 
     def __str__(self):
         return self.title
